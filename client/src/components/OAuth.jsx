2848b89a3e23ad8ce8d7b4737123eeabb25d6595
pranav <pranav@pranavs-MacBook-Air.local>
2025-05-08 01:54:27 +0530

    Removed google sign-in due to post creation issue

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -34,9 +34,10 @@ export default function OAuth() {
         }
     } 
   return (
-    <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
-        <AiFillGoogleCircle className='w-6 h-6 mr-2'/>
-        Continue with Google
-    </Button>
+    // <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
+    //     <AiFillGoogleCircle className='w-6 h-6 mr-2'/>
+    //     Continue with Google
+    // </Button>
+    <></>
   )
 }
